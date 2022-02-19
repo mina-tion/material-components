8ecd4ef6eb0648a5a2235c6de31bb45c49ed1c38
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2022-02-19 14:50:27 +0200

    create and customize switch button

---
+++ src/components/SwitchButton/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Switch from '@material-ui/core/Switch'
+import { FormControlLabel } from '@material-ui/core'
+import { styled } from '@material-ui/core'
+
+const SwitchButton = ({ component: Component, restricted, ...rest }: any): any => {
+  const IOSSwitch = styled(props => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+  ))(({ theme }) => ({
+    marginRight: 10,
+    width: 44,
+    height: 21,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+      padding: 0,
+      margin: 2.4,
+      transitionDuration: '300ms',
+      '&.Mui-checked': {
+        transform: 'translateX(22.8px)',
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          backgroundColor: '#56D01C',
+          opacity: 1,
+          border: 0,
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+          opacity: 0.5,
+        },
+      },
+      '&.Mui-focusVisible .MuiSwitch-thumb': {
+        color: '#56D01C',
+        border: '6px solid #fff',
+      },
+      '&.Mui-disabled .MuiSwitch-thumb': {
+        color: theme.palette.grey[100],
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.7,
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxSizing: 'border-box',
+      width: 16.5,
+      height: 16.5,
+      border: 0,
+      boxShadow: 'none',
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: 36,
+      backgroundColor: '#E9E9EA',
+      opacity: 1,
+      transition: theme.transitions.create(['background-color'], {
+        duration: 500,
+      }),
+    },
+  }))
+
+  return <FormControlLabel control={<IOSSwitch sx={{ m: 1 }} defaultChecked />} label="Open" />
+}
+
+export default SwitchButton
--- src/containers/Private/Main/index.tsx
+++ src/containers/Private/Main/index.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 // style
 import styles from './styles.module.scss'
 import { useObserver } from 'mobx-react'
+import SwitchButton from 'components/SwitchButton'
 
 const Main: React.FC = () => {
   return useObserver(() => (
     <main className={styles.container}>
-      <button>fg</button>
+      <SwitchButton/>
     </main>
   ))
 }
